a171f60251a5c418743909fa076897ac77f9777b
remote-torrent-6747 <abbyc@sky.com>
2021-05-03 15:04:54 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,8 +1,8 @@
 class LandmarksController < ApplicationController
 
   get '/landmarks/new' do
-    @titles = Title.all
-    @landmarks = Landmark.all
+    **@titles = Title.all
+    **@landmarks = Landmark.all
     erb :'/landmarks/new'
   end
 
