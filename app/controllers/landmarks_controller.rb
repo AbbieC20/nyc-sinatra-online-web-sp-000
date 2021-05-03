0ae229679d30e7401e1bf0ef788c54efeeb9f4a7
remote-torrent-6747 <abbyc@sky.com>
2021-05-03 15:18:42 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -5,25 +5,7 @@ class LandmarksController < ApplicationController
   end
 
   post '/landmarks/new' do
-  #   @figure = Figure.create(name: params["figure"]["name"])
-  #   # checking whether any title boxes are ticked and then associating with figure
-  #   if !params["figure"]["title_ids"].nil?
-  #     @figure.title_ids = params["figure"]["title_ids"]
-  #   end
-  #   # checking if new title was created - if yes, associating with figure
-  #   if !params["title"]["name"].empty?
-  #     @title = Title.create(name: params["title"]["name"])
-  #     @figure.titles << @title
-  #   end
-  #   # checking whether any landmark boxes are ticked and then associating with figure
-  #   if !params["figure"]["landmark_ids"].nil?
-  #     @figure.landmark_ids = params["figure"]["landmark_ids"]
-  #   end
-  #   # checking if new title was created - if yes, associating with figure
-  #   if !params["landmark"]["name"].empty?
-  #     @landmark = Landmark.create(name: params["landmark"]["name"], year_completed: params["landmark"]["year"])
-  #     @figure.landmarks << @landmark
-  #   end
+    @landmark = Landmark.create(name: params["landmark"]["name"], year_completed: ["landmark"]["year_completed"])
     redirect '/landmarks/index'
   end
 
