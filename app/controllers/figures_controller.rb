9568280f68369cb0cc18221bb320284f684e921b
remote-torrent-6747 <abbyc@sky.com>
2021-05-03 14:00:21 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,45 @@
+
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures/new' do
+    @figure = Figure.create(name: params["figure"]["name"])
+    # checking whether any title boxes are ticked and then associating with figure
+    if !params["figure"]["title_ids"].nil?
+      @figure.title_ids = params["figure"]["title_ids"]
+    end
+    # checking if new title was created - if yes, associating with figure
+    if !params["title"]["name"].empty?
+      @title = Title.create(name: params["title"]["name"])
+      @figure.titles << @title
+    end
+
+    # checking whether any landmark boxes are ticked and then associating with figure
+    if !params["figure"]["landmark_ids"].nil?
+      @figure.landmark_ids = params["figure"]["landmark_ids"]
+    end
+    # checking if new title was created - if yes, associating with figure
+    if !params["landmark"]["name"].empty?
+      @landmark = Landmark.create(name: params["landmark"]["name"], year_completed: params["landmark"]["year"])
+      @figure.landmarks << @landmark
+    end
+    redirect '/figures/index' #will list all figures
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+
 end
